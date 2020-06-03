6af610cc903bf0f660ec11a781a27518b4f7c326
yttr309 <63216289+yttr309@users.noreply.github.com>
2020-06-03 15:14:09 +0900

    2020.06.03 이정재
    2020.06.03 이정재

---
+++ Assets/Script/CatPlayer.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody rigid;
+    void Start()
+    {
+        Cat kitty = new Cat("kitty");
+        kitty.PlaySound();
+        kitty.Go();
+
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    // Update is s once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            Debug.Log("앞으로");
+            transform.position = new Vector3(
+                transform.position.x,
+                transform.position.y,
+                transform.position.z + 3f * Time.deltaTime
+                );
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            Debug.Log("뒤로");
+            transform.position = new Vector3(
+                transform.position.x,
+                transform.position.y,
+                transform.position.z - 3f * Time.deltaTime
+                );
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Debug.Log("왼쪽으로");
+            transform.position = new Vector3(
+                transform.position.x - 3f * Time.deltaTime,
+                transform.position.y,
+                transform.position.z
+                );
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Debug.Log("오른쪽으로");
+            transform.position = new Vector3(
+                transform.position.x + 3f * Time.deltaTime,
+                transform.position.y,
+                transform.position.z
+                );
+        }
+    }
+}
+
